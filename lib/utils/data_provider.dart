147f478fea1461115b8ab30e9be1eb0d8d30b28a
Jack <371997352@qq.com>
2023-03-27 23:24:10 +0800

    provider return article list

--- lib/utils/data_provider.dart
+++ lib/utils/data_provider.dart
@@ -1,14 +1,23 @@
-
 import 'package:zhihu_daily/utils/api.dart';
 import 'package:zhihu_daily/utils/request.dart';
 import 'package:dio/dio.dart';
+import 'dart:convert';
+
+import '../model/article.dart';
 
 abstract class DataProvider {
-
-  static Future<Response> getLatestArticle() async {
+  static Future<List<Article>> getLatestArticle() async {
     Response<dynamic> response = await Request().get(Api.LATEST, {});
 
-    return response;
-  }
+    List<dynamic> list = response.data['stories'];
 
+    List<Article> articleList = [];
+    for (int i = 0; i < list.length; i++) {
+      Article article = Article(
+          list[i].title, list[i].url, list[i].hint, list[i].images, list[i].id);
+      articleList.add(article);
+    }
+
+    return articleList;
+  }
 }
